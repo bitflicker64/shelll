b207f3009ab2fc6fbd7f63a4a3fcbdebeab469fa
bitflicker64 <himanshuverma15102006@gmail.com>
2025-05-15 00:01:31 +0530

    ay

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
         //(m himanshu verma aka bitflicker doing this code so yea no ai shit i am learning )
         System.out.print("$ ");
         String input;
-        Scanner o = new Scanner(System.in);
+        try (Scanner o = new Scanner(System.in));{
          /*
          * this comment is for future referance only so yea basically for me the code writer so .....
          * This loop runs as long as there is more input to read from the user.
@@ -24,15 +24,16 @@ public class Main {
          *   - This is useful when you want to keep accepting input line by line
          *     until the user explicitly stops (like in a terminal or shell).
          */
-        while (o.hasNextLine()) {
+        while (o.hasNextLine()) //after this hasLine command dont have to use close command lol 
+        {
             input = o.nextLine();
-        String command = input.split("")[0];
+        String command = input.split(" ")[0];
         switch(command){
             case "exit":
-            break;
+            System.exit(0);
             case "echo":
             // If user types "echo Hello", it prints "Hello"  // input.split(" ", 2)[1] splits only into 2 parts: ["echo", "Hello"]
-            System.out.println(input.split("",2)[1]);
+            System.out.println(input.split(" ",2)[1]);
              // Prints an error message to System.err (standard error stream)
              default : System.err.printf("%s: command not found%n", input);
              // '%s' is replaced by the whole input//'%n' adds a new line in a platform-safe way
@@ -42,3 +43,4 @@ public class Main {
     }
         }
       }
+}
