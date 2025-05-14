3a75bc01c0110e15a0b76d8ab4b065075ace6211
bitflicker64 <himanshuverma15102006@gmail.com>
2025-05-14 18:24:57 +0530

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,10 @@ public class Main {
         String input;
         Scanner o = new Scanner(System.in);
         while (true) {
-            
             input = o.nextLine();
+            if (input.equals("exit 0")){
+                break;
+            }
             System.out.println(input + ": command not found");
             System.out.print("$ ");
           }
