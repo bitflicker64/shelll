48809204345853633bcc070e4dc0c23ed4d18e43
bitflicker64 <himanshuverma15102006@gmail.com>
2025-05-14 18:21:06 +0530

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,11 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.print("$ ");
-
-        Scanner scanner = new Scanner(System.in);
+        String input;
+        Scanner o = new Scanner(System.in);
         while (true) {
             System.out.print("$ ");
-            String input = scanner.nextLine();
+            input = o.nextLine();
             System.out.println(input + ": command not found");
           }
         }
